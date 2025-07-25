a32ef8838cf5df676f30401c8a8e307f478a2062
itsmerosu <56078030+itsmerosu@users.noreply.github.com>
2025-07-25 09:18:11 +0545

    Add files via upload

---
+++ counter1.js
@@ -0,0 +1,21 @@
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+	entries.forEach( entry => {
+		const el = entry.target
+		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+			counterUp( el, {
+				duration: 2000,
+				delay: 16,
+			} )
+			el.classList.add( 'is-visible' )
+		}
+	} )
+}
+
+const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+const el = document.querySelectorAll( '.counter' )
+el.forEach(function(e){
+	IO.observe( e )
+})
